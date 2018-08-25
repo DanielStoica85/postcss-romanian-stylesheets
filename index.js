9f4b55fb6b92bc35840f5a317f6ee115d46cde77
Daniel <dansto13@yahoo.com>
2018-08-25 23:27:45 +0200

    Add first working example.

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ module.exports = postcss.plugin('postcss-romanian-stylesheets', function (opts) 
     return function (root, result) {
 
         // Transform CSS AST here
+        root.walkDecls((decl) => {
+            console.log(decl.prop);
+            console.log(decl.value);
 
+            // Properties
+            decl.prop = decl.prop.replace('fundal', 'background');
+            decl.prop = decl.prop.replace('familie-font', 'font-family');
+            // Values
+            decl.value = decl.value.replace('rosu', 'red');
+        });
+        
     };
 });
